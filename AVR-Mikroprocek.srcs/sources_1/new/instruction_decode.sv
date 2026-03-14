aca5b830bb53c3355a6273f22dd2f93280791e4d
Jan Mrzygłód <mrzyglodjan@protonmail.com>
2026-03-14 11:37:33 +0100

    test

---
+++ AVR-Mikroprocek.srcs/sources_1/new/instruction_decode.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/14/2026 11:34:14 AM
+// Design Name: 
+// Module Name: instruction_decode
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instruction_decode(
+
+    );
+endmodule
